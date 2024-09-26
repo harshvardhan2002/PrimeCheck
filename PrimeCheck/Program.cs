ae7a36daed44a03e86d1a41aa32e9959276d1514
Harshvardhan Gupta <76556011+harshvardhan2002@users.noreply.github.com>
2024-09-26 11:47:12 +0530

    Add files via upload

---
+++ PrimeCheck/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			int number, count = 0;
+            Console.WriteLine("Write the num to check: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number <= 1)
+            {
+                Console.WriteLine("num is not a prime num");
+            }
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("num is a prime num");
+            }
+            else
+            {
+                Console.WriteLine("num is not a prime num");
+            }
+        }
+		
+    }
+}
---
+++ PrimeCheck/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PrimeCheck")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrimeCheck")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("845c9b40-1074-44d6-8b12-57e3d40f4266")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
